bc273ad3b8ff4e5b1a32a84d5c7ef6e7dd0f1a7f
Daniel N. Blaschke <40176390+dblaschke-LANL@users.noreply.github.com>
2026-06-26 11:15:15 -0600

    include copyright notices in all source files

--- postprocessing/simple_postprocess.py
+++ postprocessing/simple_postprocess.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 ### python 3.8 or higher required
 ### also: plot_model_stress=True requires PyDislocDyn >=1.2.9
+# © 2026. Triad National Security, LLC. All rights reserved.
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos National Laboratory (LANL), 
+# which is operated by Triad National Security, LLC for the U.S. Department of Energy/National Nuclear Security Administration.
+# All rights in the program are reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+# Security Administration. The Government is granted for itself and others acting on its behalf a nonexclusive, paid-up,
+# irrevocable worldwide license in this material to reproduce, prepare. derivative works, distribute copies to the public, perform
+# publicly and display publicly, and to permit others to do so.
 import sys
 import os
 import numpy as np
